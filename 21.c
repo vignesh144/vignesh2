527a1defd34f8be89c6057d26881073363482c69
vignesh144 <35727401+vignesh144@users.noreply.github.com>
2018-03-16 16:31:05 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void
+main () 
+{
+int d, n, f, a = 0, sum = 0, e = 0;
+scanf ("%d", &n);
+scanf ("%d", &d);
+scanf ("%d", &f);
+if (n >= 0 && f >= 0)
+    {
+for (int i = 0; i < n; i++)
+	{
+a = a + d;
+sum = sum + a;
+} 
+printf ("%d", sum);
+}
+  else
+    {
+return e;
+}
+}
